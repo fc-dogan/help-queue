78e058bf84a1493422c9b7d12f1b2c37bf0f3b33
Fatma <fceyda@gmail.com>
2020-09-08 16:07:31 -0700

    update App component to render the TicketControl component

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import TicketList from "./TicketList";
+import NewTicketForm from "./NewTicketForm";
 
 class TicketControl extends React.Component {
 
