07c5e9190ffa31c4756a13ba43d11cd52e4457db
Fatma <fceyda@gmail.com>
2020-09-10 10:50:15 -0700

    add update buttton with onClick handler to ticketDetail

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -2,14 +2,15 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 function TicketDetail(props){
-  const { ticket, onClickingDelete } = props;
+  const { ticket, onClickingDelete, onClickingEdit } = props;
   //  object destructuring to derive the ticket object from our props
   return(
     <React.Fragment>
       <p>Ticket detail</p>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
-      <button onClick={() => onClickingDelete(ticket.id) }>Close Ticket</button>
+      <button onClick={() => onClickingDelete(ticket.id) }>Update Ticket</button>
+      <button onClick={() => onClickingEdit(ticket.id) }>Close Ticket</button>
       <hr/>
     </React.Fragment>
   );
@@ -17,7 +18,8 @@ function TicketDetail(props){
 
 TicketDetail.propTypes = {
   ticket: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 }
 
 export default TicketDetail;
