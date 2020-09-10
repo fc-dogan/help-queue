61b8180bd71bdb04bb271a75195e5fa3b13dc8f3
Fatma <fceyda@gmail.com>
2020-09-10 09:51:45 -0700

    add a delete button and propType to ticketDetail

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -2,20 +2,22 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 function TicketDetail(props){
-  const { ticket } = props;
+  const { ticket, onClickingDelete } = props;
   //  object destructuring to derive the ticket object from our props
   return(
     <React.Fragment>
       <p>Ticket detail</p>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <button onClick={() => onClickingDelete(ticket.id) }>Close Ticket</button>
       <hr/>
     </React.Fragment>
   );
 }
 
 TicketDetail.propTypes = {
-  ticket: PropTypes.object
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func
 }
 
 export default TicketDetail;
